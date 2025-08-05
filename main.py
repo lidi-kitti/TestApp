b5f65e4fbfc5d71797100704db0d4bf0db0d7035
Lydia <lida.presnukhina@mail.ru>
2025-08-05 14:09:45 +0300

    Fihish program

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from dependencies import get_current_user
 from init_data import init_test_data
 
 
-@asynccontextmanager
+#@asynccontextmanager
 async def lifespan(app: FastAPI):
     # Удаляем старую базу данных для пересоздания
     if os.path.exists("auth_system.db"):
-        os.remove("auth_system.db")
-        print("Старая база данных удалена")
+       os.remove("auth_system.db")
+       print("Старая база данных удалена")
     # Создание таблиц при запуске
     Base.metadata.create_all(bind=engine)
     init_test_data()
@@ -45,6 +45,9 @@ app.include_router(router, prefix="/api")
 async def protected_route(current_user = Depends(get_current_user)):
     return {"message": "Доступ разрешен", "user": current_user.email}
 
+@app.get("/")
+async def root():
+    return {"message": "Главная страница"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
